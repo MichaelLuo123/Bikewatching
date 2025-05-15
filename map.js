ca6b9893a9ab73e93f43f0cc1cf6741ab0931bc2
Michael Luo <mluo.lion@gmail.com>
2025-05-15 04:35:55 -0700

    Update map.js

--- map.js
+++ map.js
@@ -57,9 +57,31 @@ map.on('load', async () => {
 
     // Await JSON fetch
         const jsonData = await d3.json(jsonurl);
+        const trafficUrl = 'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv';
+        const trips = await d3.csv(trafficUrl);
+        console.log('Loaded Trips:', trips.length); 
         let stations = jsonData.data.stations;
         console.log('Stations Array:', stations);
         console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+        const departures = d3.rollup(
+            trips,
+            v => v.length,
+            d => d.start_station_id
+          );
+          
+          const arrivals = d3.rollup(
+            trips,
+            v => v.length,
+            d => d.end_station_id
+          );
+          
+          stations = stations.map((station) => {
+            let id = station.short_name;
+            station.arrivals = arrivals.get(id) ?? 0;
+            station.departures = departures.get(id) ?? 0;
+            station.totalTraffic = station.arrivals + station.departures;
+            return station;
+          });
         const circles = svg
         .selectAll('circle')
         .data(stations)
